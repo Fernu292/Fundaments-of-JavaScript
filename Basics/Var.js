3a79dd25d91831bca63c8d5a1c8351ecf5901754
Fernu292 <fernu292@gmail.com>
2021-08-18 12:18:43 -0500

    Basics
    Basics JavaSript concepts

---
+++ Basics/Var.js
@@ -0,0 +1,36 @@
+//The variables in JavaScript can declarate with some reservated words
+//The variables in JS are dinamics, thats mean, that the variable do not start 
+//with the type of data 
+
+//Data type 
+
+    //int = interger number
+    //char = a caracter
+    //bool = a logig variable, True or False
+    //string = an array of characters
+    //float = number with decimal 
+    //double = a long number 
+
+
+
+//Variables with var
+//this variables have a scope in all document 
+var nombre1 = "Luis Fernando";
+
+
+//Variables with let
+//This variables have a limitated scope and weigh less in memory than var 
+let nombre2 = "Juan";
+
+
+//Const this type of variable can not change their value one time its declared 
+
+const nombre3 = "Pepe";
+
+//Show data in console
+//For this taks we will use console.log();
+
+
+console.log(nombre1);
+console.log(nombre2);
+console.log(nombre3);
---
+++ Basics/aritmetics.js
@@ -0,0 +1,37 @@
+//We also make a simple operation in JS 
+//First we declarate two variables 
+
+let num1 = 5;
+let num2 = 8;
+
+//Adition, only we use "+"
+
+let adittion;
+
+adittion = num1+num2;
+
+console.log(adittion);
+
+//subtraction, only use "-"
+
+let subtraction;
+
+subtraction = num2-num1;
+
+console.log(subtraction);
+
+//multiplication for this we use "*"
+
+let multi;
+
+multi = num1*num2;
+
+console.log(multi);
+
+//Division, only use "/"
+
+let division;
+
+division = num1/num2;
+
+console.log(division);
